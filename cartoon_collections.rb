3b24b2d5fa96ad362aaa1eccc611784705ed2b7b
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-04-16 16:06:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,8 @@ end
 
 def summon_captain_planet(array)  # code an argument here
   # Your code here
-  array.map {|ele| ele.upcase + "!"}
+  result = array.map {|ele| ele.upcase + "!"}
+  result
 end
 
 def long_planeteer_calls# code an argument here
